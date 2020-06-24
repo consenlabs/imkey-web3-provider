7011be4f31c0749fc88e455d459498a5a92c1a72
xiemener <xielamenla@qq.com>
2020-06-24 11:36:39 +0800

    Add signTransaction and signMessgae test

--- src/demo.ts
+++ src/demo.ts
@@ -14,7 +14,28 @@ btnBalance.addEventListener('click', (e) => {
         .then(console.log);
 });
 
+const btnSignTransaction = document.createElement('button');
+btnSignTransaction.innerText = "Sign Transaction";
+btnSignTransaction.addEventListener('click', (e) => {
+    web3.eth.signTransaction({
+        from: "0xEB014f8c8B418Db6b45774c326A0E64C78914dC0",
+        gasPrice: "20000000000",
+        gas: "21000",
+        to: '0x3535353535353535353535353535353535353535',
+        value: "1000000000000000000",
+        data: ""
+    }).then(console.log);
+});
+
+const btnSignMessage = document.createElement('button');
+btnSignMessage.innerText = "Sign Message";
+btnSignMessage.addEventListener('click', (e) => {
+    web3.eth.sign("Hello world", "0x11f4d0A3c12e86B4b5F39B213F7E19D048276DAe")
+        .then(console.log);
+});
+
 // document.appendChild(btn);
 document.body.append(btn);
 document.body.append(btnBalance);
-
+document.body.append(btnSignTransaction);
+document.body.append(btnSignMessage);
--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,71 @@ export default class ImKeyProvider extends Web3 {
     constructor(provider: string) {
         super(Web3.givenProvider || provider);
         this.eth.requestAccounts = imKeyRequestAccounts;
+        this.eth.signTransaction = imKeySignTransaction;
+        this.eth.sign = imKeySignMessage;
     }
 }
 
 function imKeyRequestAccounts() {
     return new Promise<string[]>((resolve, reject) => {
         postData(IMKEY_MANAGER_ENDPOINT, {
-                "jsonrpc": "2.0",
-                "method": "eth.getAddress",
-                "params": {
+            "jsonrpc": "2.0",
+            "method": "eth.getAddress",
+            "params": {
+                "path": "m/44'/60'/0'/0/0"
+            },
+            "id": requestId++
+        }
+        ).then((ret) => {
+            console.log("request address: ", JSON.stringify(ret));
+        })
+    })
+}
+
+function imKeySignTransaction() {
+    return new Promise<any>((resolve, reject) => {
+        postData(IMKEY_MANAGER_ENDPOINT, {
+            "jsonrpc": "2.0",
+            "method": "eth.signTransaction",
+            "params": {
+                "transaction": {
+                    "data": "",
+                    "gasLimit": 189000,
+                    "gasPrice": 20000000008,
+                    "nonce": 8,
+                    "to": "0x3535353535353535353535353535353535353535",
+                    "value": 512,
+                    "chainId": "28",
                     "path": "m/44'/60'/0'/0/0"
                 },
-                "id": requestId++
-            }
+                "preview": {
+                    "payment": "0.01 ETH",
+                    "receiver": "0xE6F4142dfFA574D1d9f18770BF73814df07931F3",
+                    "sender": "0x6031564e7b2F5cc33737807b2E58DaFF870B590b",
+                    "fee": "0.0032 ether"
+                },
+                "txHash": "0x09fa41c4d6b92482506c8c56f65b217cc3398821caec7695683110997426db01"
+            },
+            "id": 10
+        }
+        ).then((ret) => {
+            console.log("sign transaction: ", JSON.stringify(ret));
+        })
+    })
+}
+
+function imKeySignMessage() {
+    return new Promise<string>((resolve, reject) => {
+        postData(IMKEY_MANAGER_ENDPOINT, {
+            "jsonrpc": "2.0",
+            "method": "eth.signMessage",
+            "params": {
+                "data": "Hello imKey",
+                "sender": "0x6031564e7b2F5cc33737807b2E58DaFF870B590b",
+                "path": "m/44'/60'/0'/0/0"
+            },
+            "id": 11
+        }
         ).then((ret) => {
             console.log("request address: ", JSON.stringify(ret));
         })
