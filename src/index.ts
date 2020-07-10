bb8a20e7e403d12075c3c5092423c9204aed569a
Neal Xu <imxuneal@gmail.com>
2020-07-10 10:34:20 +0800

    WIP add the request method

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,65 @@
 import Web3 from 'web3';
 import * as rlp from 'rlp';
-import { TransactionConfig, RLPEncodedTransaction } from "web3-eth"
+import {TransactionConfig, RLPEncodedTransaction} from "web3-eth"
+import {HttpProvider, WebsocketProvider} from "web3-core";
 
+
+interface RequestArguments {
+    method: string;
+    params?: any[];
+}
 
 const IMKEY_MANAGER_ENDPOINT = "http://localhost:8081/api/imkey";
 const IMKEY_ETH_PATH = "m/44'/60'/0'/0/0";
 let requestId = 0;
 
-export default class ImKeyProvider extends Web3 {
+export default class ImKeyProvider{
+    infuraProvider: WebsocketProvider
+
     constructor(provider: string) {
-        super(Web3.givenProvider || provider);
-        this.eth.requestAccounts = this.imKeyRequestAccounts;
-        this.eth.signTransaction = this.imKeySignTransaction.bind(this);
-        this.eth.sign = this.imKeySignMessage;
+        // super(Web3.givenProvider || provider);
+        this.infuraProvider = new WebsocketProvider(provider);
+        // this.eth.requestAccounts = this.imKeyRequestAccounts;
+        // this.eth.signTransaction = this.imKeySignTransaction.bind(this);
+        // this.eth.sign = this.imKeySignMessage;
+    }
+
+    request(args: RequestArguments): Promise<any> {
+        switch (args.method) {
+            case 'eth_requestAccounts':
+                return this.imKeyRequestAccounts()
+            default:
+
+                return new Promise<unknown>(((resolve, reject) => {
+
+                    let payload = {
+                        jsonrpc: "2.0",
+                        method: args.method,
+                        params: args.params,
+                        id: requestId++
+                    };
+                    this.infuraProvider.send(payload, (err, ret) => {
+                        if (err!=null) {
+                            reject(err);
+                        } else {
+                            resolve(ret);
+                        }
+                    })
+                }))
+
+        }
     }
 
     imKeyRequestAccounts(callback?: (error: Error, result: string[]) => void) {
         return new Promise<string[]>((resolve, reject) => {
             postData(IMKEY_MANAGER_ENDPOINT, {
-                "jsonrpc": "2.0",
-                "method": "eth.getAddress",
-                "params": {
-                    "path": IMKEY_ETH_PATH
-                },
-                "id": requestId++
-            }
+                    "jsonrpc": "2.0",
+                    "method": "eth.getAddress",
+                    "params": {
+                        "path": IMKEY_ETH_PATH
+                    },
+                    "id": requestId++
+                }
             ).then((ret) => {
                 callback?.(ret.error, ret.result.address);
                 if (ret.result == null) {
@@ -56,34 +91,34 @@ export default class ImKeyProvider extends Web3 {
 
         return new Promise<RLPEncodedTransaction>((resolve, reject) => {
             postData(IMKEY_MANAGER_ENDPOINT, {
-                "jsonrpc": "2.0",
-                "method": "eth.signTransaction",
-                "params": {
-                    "transaction": {
-                        "data": transactionConfig.data,
-                        "gasLimit": gasLimit,
-                        "gasPrice": transactionConfig.gasPrice,
-                        "nonce": transactionConfig.nonce,
-                        "to": transactionConfig.to,
-                        "value": transactionConfig.value,
-                        "chainId": transactionConfig.chainId,
-                        "path": IMKEY_ETH_PATH
+                    "jsonrpc": "2.0",
+                    "method": "eth.signTransaction",
+                    "params": {
+                        "transaction": {
+                            "data": transactionConfig.data,
+                            "gasLimit": gasLimit,
+                            "gasPrice": transactionConfig.gasPrice,
+                            "nonce": transactionConfig.nonce,
+                            "to": transactionConfig.to,
+                            "value": transactionConfig.value,
+                            "chainId": transactionConfig.chainId,
+                            "path": IMKEY_ETH_PATH
+                        },
+                        "preview": {
+                            "payment": transactionConfig.value!.toString() + " ETH",
+                            "receiver": transactionConfig.to,
+                            "sender": transactionConfig.from,
+                            "fee": fee
+                        }
                     },
-                    "preview": {
-                        "payment": transactionConfig.value!.toString() + " ETH",
-                        "receiver": transactionConfig.to,
-                        "sender": transactionConfig.from,
-                        "fee": fee
-                    }
-                },
-                "id": requestId++
-            }
+                    "id": requestId++
+                }
             ).then((ret) => {
                 if (ret.result == null) {
                     reject(ret.error);
                 } else {
                     var txData = ret.result.txData;
-                    if(!ret.result.txData.startsWith("0x")){
+                    if (!ret.result.txData.startsWith("0x")) {
                         txData = "0x" + txData;
                     }
                     const decoded = rlp.decode(txData, true);
@@ -97,8 +132,11 @@ export default class ImKeyProvider extends Web3 {
                             to: transactionConfig.to!.toString(),
                             value: transactionConfig.value!.toString(),
                             input: transactionConfig.data!.toString(),
+                            // @ts-ignore
                             r: this.utils.bytesToHex(decoded.data[7]),
+                            // @ts-ignore
                             s: this.utils.bytesToHex(decoded.data[8]),
+                            // @ts-ignore
                             v: Number(this.utils.bytesToHex(decoded.data[6])).toString(),
                             hash: ret.result.hash,
                         }
@@ -119,15 +157,15 @@ export default class ImKeyProvider extends Web3 {
 
         return new Promise<string>((resolve, reject) => {
             postData(IMKEY_MANAGER_ENDPOINT, {
-                "jsonrpc": "2.0",
-                "method": "eth.signMessage",
-                "params": {
-                    "data": dataToSign,
-                    "sender": address,
-                    "path": IMKEY_ETH_PATH
-                },
-                "id": requestId++
-            }
+                    "jsonrpc": "2.0",
+                    "method": "eth.signMessage",
+                    "params": {
+                        "data": dataToSign,
+                        "sender": address,
+                        "path": IMKEY_ETH_PATH
+                    },
+                    "id": requestId++
+                }
             ).then((ret) => {
                 callback?.(ret.error, ret.result.signature);
                 if (ret.result == null) {
